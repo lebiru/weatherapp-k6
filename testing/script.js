32b4dae971266ae76f3f356480d77030c5648f48
Lebiru <bilal.akabi@gmail.com>
2021-07-27 21:55:13 -0400

    git cache remove bin/obj, k6io testing

--- testing/script.js
+++ testing/script.js
@@ -1,7 +1,14 @@
 import http from 'k6/http';
-import { sleep } from 'k6';
+import { sleep, check } from 'k6';
+export let options = {
+  insecureSkipTLSVerify: true,
+  stages: [
+    { duration: '30s', target: 5}
+  ]
+};
 
 export default function() {
-  http.get('https://localhost:5001/weatherforecast');
+  let res = http.get('https://localhost:5001/weatherforecast');
+  check(res, {'status was 200': (r) => r.status == 200 });
   sleep(1);
 }
